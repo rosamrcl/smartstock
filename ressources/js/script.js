f56129f852b321e1f11a6031587a2f4ce88cd21b
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-06-30 14:40:55 -0300

    .

---
+++ ressources/js/script.js
@@ -0,0 +1,9 @@
+//menu
+
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active'); 
+}
