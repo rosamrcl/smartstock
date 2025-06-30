34e93b69c2a496586349119bc0b403537db067af
Rosa <costurezati@gmail.com>
2025-06-30 19:25:04 -0300

    .

---
+++ cadastrogerencia.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartStock</title>
+    <link rel="stylesheet" href="./ressources/css/style.css">
+    <link rel="stylesheet" href="./ressources/css/header.css">
+    <link rel="stylesheet" href="./ressources/css/form.css">
+    <link rel="stylesheet" href="./ressources/css/cadastrogerencia.css">
+    <link rel="stylesheet" href="./ressources/css/media.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <a href="#home" class="home"><i class="fa-solid fa-house"></i></a>
+        <img src="./ressources/img/smartstock.png" alt="logo">
+        <nav class="navbar">
+            <a href=""><i class="fas fa-search" id="search-icon"></i></a>
+            <a href=""><i class="fa-solid fa-user"></i></a>
+            <a href=""><i class="fa-solid fa-bell"></i></a>
+        </nav>
+        <div class="icons">
+            <i class="fas fa-bars" id="menu-bars"></i>
+        </div>
+    </header>
+
+    <section class="cadastrogerencia">
+        <div class="form-produto">
+            <div class="form-container-produto">
+                <form action="" method="post">
+                    <h3>Cadastro do produto</h3>
+                    <label for="productname">Nome produto</label>
+                    <input type="text" name="productname" id="">
+                    <label for="descricao">Descrição</label>
+                    <input type="text" name="descricao">
+                    <label for="status">Status</label>
+                    <select name="status" id="">
+                        <option value="funcionando">Funcionando</option>
+                        <option value="funcionando">Com defeito</option>
+                        <option value="funcionando">Em uso</option>
+                    </select>
+                    <label for="quantidade">Quantidade</label>
+                    <input type="number" name="quantidade" id="">
+                    <input type="submit" value="Adicionar" class="btn" name="adicionar">
+                </form>
+            </div>
+        </div>
+        <div class="table-container">
+            <h3>Gerenciar Produtos</h3>
+            <div class="tabela">
+                <div class="tab-buttons">
+                    <button class="tab-button active" onclick="openTab('tab1')">Estoque</button>
+                    <button class="tab-button" onclick="openTab('tab2')">Manutenção</button>
+                    <button class="tab-button" onclick="openTab('tab3')">Em uso</button>
+                </div>
+                <div id="tab1" class="tab-content active">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Status</th>
+                                <th>Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Dado 1-1</td>
+                                <td>Dado 1-2</td>
+                                <td>Dado 1-3</td>
+                                <td>Dado 1-4</td>
+                                <td>Dado 1-5</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div id="tab2" class="tab-content">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Status</th>
+                                <th>Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>                
+                            <tr>
+                                <td>Info A2</td>
+                                <td>Info B2</td>
+                                <td>Info B2</td>
+                                <td>Info B2</td>
+                                <td>Info B2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div id="tab3" class="tab-content">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nome</th>
+                                <th>Descrição</th>
+                                <th>Status</th>
+                                <th>Quantidade</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Produto X</td>
+                                <td>R$ 10,00</td>
+                                <td>5</td>
+                                <td>Produto X</td>
+                                <td>R$ 10,00</td>
+                                <td>5</td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+
+    </section>
+
+    <script src="./ressources/js/script.js"></script>
+
+</body>
+
+</html>
--- ressources/js/script.js
+++ ressources/js/script.js
@@ -7,3 +7,21 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active'); 
 }
+
+function openTab(tabId) {
+            // Esconde todas as tabelas
+            var tabContents = document.getElementsByClassName('tab-content');
+            for (var i = 0; i < tabContents.length; i++) {
+                tabContents[i].classList.remove('active');
+            }
+            
+            // Remove a classe 'active' de todos os botões
+            var tabButtons = document.getElementsByClassName('tab-button');
+            for (var i = 0; i < tabButtons.length; i++) {
+                tabButtons[i].classList.remove('active');
+            }
+            
+            // Mostra a tabela selecionada e marca o botão como ativo
+            document.getElementById(tabId).classList.add('active');
+            event.currentTarget.classList.add('active');
+        }
