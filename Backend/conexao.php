1efc7e998efed39c3e17518f9759c04d8276f771
BananaSpritee <164941682+BananaSpritee@users.noreply.github.com>
2025-07-01 17:24:48 -0300

    Mudança no arquivo conexão para criação do banco de dados automatica caso não exista.

--- Backend/conexao.php
+++ Backend/conexao.php
@@ -5,6 +5,8 @@ $pass = '';
 $db = 'smartstock';
 
 try {
+    $pdo = new PDO("mysql:host=$host;charset=utf8", $user, $pass);
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS smartstock");
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
