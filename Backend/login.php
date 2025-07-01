527e7cf42b1301314c3ded5fde62b865f4bd10e6
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-07-01 15:41:45 -0300

    Criaçao e validação do LOGIN

---
+++ Backend/login.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+include __DIR__ . '/conexao.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'] ?? '';
+    $senha = $_POST['senha'] ?? '';
+
+    if (empty($email) || empty($senha)) {
+        $_SESSION['erro_login'] = "Preencha todos os campos.";
+        header('Location: ../Frontend/login.php');
+        exit;
+    }
+
+
+    $stmt = $pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_hash($senha, $user['senha'])) {
+        $_SESSION['usuario'] = [
+            'id_user' => $user['id_user'],
+            'name' => $user['name'],
+            'sobrenome' => $user['sobrenome'],
+            'email' => $user['email']
+        ];
+
+        header('Location: ../Frontend/home.php');
+        exit;
+    } else {
+        $_SESSION['erro_login'] = "Usuário ou senha inválidos.";
+        header('Location: ../Frontend/login.php');
+        exit;
+    }
+} else {
+    header('Location: ../Frontend/login.php');
+    exit;
+}
+
+?>
