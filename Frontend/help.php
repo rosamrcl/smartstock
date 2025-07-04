8f620123f397e35f21deb4409cb6ac4a06b17390
Rosa <costurezati@gmail.com>
2025-07-04 20:05:07 -0300

    inclusão pagina de ajuda com padronização do css

---
+++ Frontend/help.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartStock</title>
+    <link rel="stylesheet" href="./ressources/css/style.css">
+    <link rel="stylesheet" href="./ressources/css/header.css">
+    <link rel="stylesheet" href="./ressources/css/help.css">
+    <link rel="stylesheet" href="./ressources/css/media.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <?php
+    include __DIR__ . '/includes/header.php';
+    ?>
+    <div class="flex">
+        <div class="heading">
+            <h1> Como podemos ajudar você?</h1>
+        </div>
+
+        <div class="container">
+            <form action="" method="post">
+                <div class="input-group">
+                    <input type="text" placeholder="Digite seu nome">
+                    <input type="email" placeholder="Digite seu email">
+                </div>
+                <textarea placeholder="Digite seu problema"></textarea>
+                <div class="file-upload-group">
+                    <label for="arquivo">Enviar foto ou PDF</label>
+                    <input type="file" id="arquivo" name="arquivo">
+                    <button type="submit" class="btn"> <i class="fa-solid fa-upload"></i></button>
+                </div>
+                <input type="submit" class="btn" value="Enviar">
+            </form>
+        </div>
+    </div>
+    <?php
+    include __DIR__ . '/includes/footer.php';
+    ?>
+</body>
+<script src="/Frontend/ressources/js/script.js"></script>
+
+</html>
--- Frontend/login.php
+++ Frontend/login.php
@@ -13,18 +13,6 @@
 </head>
 <body>
     
-    <header>
-        <a href="#home" class="home"><i class="fa-solid fa-house"></i></a>
-        <img src="./ressources/img/smartstock.png" alt="logo">
-        <nav class="navbar">  
-            <a href=""><i class="fas fa-search" id="search-icon"></i></a>
-            <a href=""><i class="fa-solid fa-user"></i></a>
-            <a href=""><i class="fa-solid fa-bell"></i></a>
-        </nav>
-        <div class="icons">
-            <i class="fas fa-bars" id="menu-bars"></i>
-        </div>        
-    </header>
 
 <body>
     <?php
