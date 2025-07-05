3f3c0353ff2a1fa4bd435bb846174d6b9b3802c2
Rosa <costurezati@gmail.com>
2025-07-05 09:26:05 -0300

    Estilização do botão input file

--- Frontend/help.php
+++ Frontend/help.php
@@ -9,7 +9,9 @@
     <link rel="stylesheet" href="./ressources/css/header.css">
     <link rel="stylesheet" href="./ressources/css/help.css">
     <link rel="stylesheet" href="./ressources/css/media.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
@@ -29,9 +31,10 @@
                 </div>
                 <textarea placeholder="Digite seu problema"></textarea>
                 <div class="file-upload-group">
-                    <label for="arquivo">Enviar foto ou PDF</label>
-                    <input type="file" id="arquivo" name="arquivo">
-                    <button type="submit" class="btn"> <i class="fa-solid fa-upload"></i></button>
+                    <label for="arquivo" class="custom-file-upload">
+                        <i class="fa-solid fa-upload"></i> Enviar foto ou PDF
+                    </label>
+                    <input type="file" id="arquivo" name="arquivo" class="hidden-file-input">
                 </div>
                 <input type="submit" class="btn" value="Enviar">
             </form>
