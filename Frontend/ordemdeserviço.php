93f112ee947272d4af2b47c1ffd8b6e580f93609
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-07-07 16:51:48 -0300

    atualização css para foto de perfil ficar redonda

--- Frontend/ordemdeserviço.php
+++ Frontend/ordemdeserviço.php
@@ -18,6 +18,8 @@ if (!isset($_SESSION['id_user'])) {
     <link rel="stylesheet" href="./ressources/css/header.css">
     <link rel="stylesheet" href="./ressources/css/form.css">
     <link rel="stylesheet" href="./ressources/css/media.css">
+    <link rel="stylesheet" href="./ressources/css/updateperfil.css">
+    <link rel="stylesheet" href="./ressources/css/home.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
         integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
