b6dfe65b12f3dc76ea64b4dec2473ca45b737f97
Rosa <costurezati@gmail.com>
2025-07-31 07:05:43 -0300

    correções css

--- Frontend/includes/head.php
+++ Frontend/includes/head.php
@@ -1,10 +1,6 @@
 <!-- Links CSS Padrão - SmartStock -->
+<link rel="shortcut icon" href="../ressources/img/favicon-32x32.png" type="image/x-icon">
 <link rel="stylesheet" href="./ressources/css/style.css">
-<link rel="stylesheet" href="./ressources/css/alerts.css">
-<link rel="stylesheet" href="./ressources/css/login-visibility-fix.css">
-<link rel="stylesheet" href="./ressources/css/login-alignment.css">
-<link rel="stylesheet" href="./ressources/css/header-improvements.css">
-<link rel="stylesheet" href="./ressources/css/updateperfil.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
     integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" />
--- Frontend/suporte.php
+++ Frontend/suporte.php
@@ -46,7 +46,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
